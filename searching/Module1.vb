b20fcc2adeb36bebcb9bea4d226ae522060f6b75
RIPT1DE <siddiqi.talha1@gmail.com>
2017-12-06 19:04:48 +0500

    initial

---
+++ searching/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim arr() As Integer = {1, 3, 5, 7, 10, 13, 15, 18, 22}
+        Console.WriteLine(binarySearch(arr, 15, 0, arr.Length - 1))
+        Console.ReadLine()
+    End Sub
+
+    Function binarySearch(ByRef arr() As Integer, ByVal input As Integer, ByVal min As Integer, ByVal max As Integer)
+        If input <= arr(max) And input >= arr(min) Then
+            Dim mid As Integer = (max + min) / 2
+            If input = arr(mid) Then
+                Return mid
+            ElseIf input < arr(mid) Then
+                Return binarySearch(arr, input, min, mid)
+            ElseIf input > arr(mid) Then
+                Return binarySearch(arr, input, mid, max)
+            End If
+        Else
+            Return -1
+        End If
+    End Function
+End Module
---
+++ searching/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("searching")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("searching")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d02f2993-12ca-4099-9327-5655419bd035")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
