4ef954a138ed36350492ab056a52d1d071d61752
RIPT1DE <siddiqi.talha1@gmail.com>
2017-12-06 19:25:33 +0500

    added serial search

--- searching/Module1.vb
+++ searching/Module1.vb
@@ -6,6 +6,22 @@
         Console.ReadLine()
     End Sub
 
+
+    Function serialSearch(ByVal arr() As Integer, ByVal input As Integer)
+        Dim found As Boolean = False
+        Dim index As Integer
+        Do While found = False And index <= arr.Length - 1
+            If arr(index) = input Then
+                found = True
+            End If
+            index += 1
+        Loop
+        If found = True Then
+            Return index
+        Else
+            Return -1
+        End If
+    End Function
     Function binarySearch(ByRef arr() As Integer, ByVal input As Integer, ByVal min As Integer, ByVal max As Integer)
         If input <= arr(max) And input >= arr(min) Then
             Dim mid As Integer = (max + min) / 2
